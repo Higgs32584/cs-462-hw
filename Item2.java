f1c003fde8d9cfb6004aeef31b699483c39299f3
Higgs32584 <mjhiggins2@loyola.edu>
2022-02-24 13:05:11 -0500

    stuff

--- Item2.java
+++ Item2.java
@@ -1,4 +1,7 @@
 import java.util.Random;
+
+import javax.sound.midi.SysexMessage;
+
 import java.util.*;
 class Item2 implements Comparable<Item2>{
     public int weight;
@@ -34,6 +37,7 @@ class Item2 implements Comparable<Item2>{
     public static void main(String args[]) {
         Random rand= new Random();
         int num_items= rand.nextInt(101)+1;
+        int max_weight=10;
         List<Item2> arr= new ArrayList<Item2>();
         for(int i=0;i<num_items;i++){
         int random= randnum();
@@ -43,31 +47,43 @@ class Item2 implements Comparable<Item2>{
         }
         int[][] weight_vals= new int[num_items][10];
         int[][] value_vals= new int[num_items][10];
-        int max_weight=10;
+        int temp_max_weight=10;
         int d=4;
         int weight=0;
         int value=0;
         for(int i=0;i<num_items;i++){
-        d=i;
-        weight=0;
-        value=0;
-        while(d != -1 && weight<= max_weight+ arr.get(d).weight){
-         Item2 cur_item=arr.get(d);
-        if (max_weight >= (cur_item.weight+weight)){
-        weight+=cur_item.weight;
-        value+=cur_item.value;
+            System.out.println("Item"+i+": ");
+            for(int j=0;j<max_weight;j++){
+                d=i;
+                temp_max_weight=j;
+                weight=0;
+                value=0;
+                System.out.print("Item: "+i+"|");
+                while(d != -1 && weight<= temp_max_weight+ arr.get(d).weight){
+                    Item2 cur_item=arr.get(d);
+                    if (temp_max_weight >= (cur_item.weight+weight)){
+                        weight+=cur_item.weight;
+                        value+=cur_item.value;
         
         }
-        d--;
+                d--;
         }
-        if( i == 0){
-                weight_vals[0][0]= weight;
-                value_vals[0][0]= value;
+                if( i == 0){
+                    weight_vals[0][0]= weight;
+                    value_vals[0][0]= value;
+                    System.out.print(value+", yes");
         }
-        else if(value <= value_vals[i-1][0]){
-            
+                else if(value < value_vals[i-1][0]){
+                    weight_vals[i][0] = weight_vals[i-1][0];
+                    value_vals[i][0] = value_vals[i-1][0];
+                    System.out.print(value_vals[i-1][0]+", no");
+                }
+                else{
+                    weight_vals[i][0] = weight;
+                    value_vals[i][0] = value;
+                    System.out.print(value+", yes");
         }
-        System.out.println("Item: "+i+"|"+value);
+    }
     }  
 
     }
