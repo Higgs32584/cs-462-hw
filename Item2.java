a040ba540f488b74c3a9e89731b94b7e9e90378b
Higgs32584 <mjhiggins2@loyola.edu>
2022-02-24 13:22:12 -0500

    new stuff

--- Item2.java
+++ Item2.java
@@ -1,50 +1,9 @@
-import java.util.Random;
-
-import javax.sound.midi.SysexMessage;
-
 import java.util.*;
-class Item2 implements Comparable<Item2>{
-    public int weight;
-    public int value;
-    public double density;
-    public Item2(int weight,int value){
-        this.weight=weight;
-        this.value=value;
-        density= (double)value/(double)weight;
-    }
-
-
-    public static int randnum(){
-        Random rand = new Random();
-        int int_random = rand.nextInt(7)+ 1;
-        return int_random;
-    }
-   public int compareTo(Item2 i){
-        if(this.density == i.density)
-                return 0;
-         if (this.density > i.density){
-                return 1;
-         }
-         return -1;
-    }
-   @Override
-   public String toString(){
-   return "Density: "+density+"Weight: "+weight+"Value:" + value;
-
-   }
-
-
-    public static void main(String args[]) {
-        Random rand= new Random();
-        int num_items= rand.nextInt(101)+1;
+class dynamic{
+    public static void main(List<Item> args) {
+        int num_items= args.size();
         int max_weight=10;
-        List<Item2> arr= new ArrayList<Item2>();
-        for(int i=0;i<num_items;i++){
-        int random= randnum();
-        int random2= randnum();
-        Item2 I = new Item2(random,random2);
-        arr.add(I);
-        }
+        List<Item> arr= args;
         int[][] weight_vals= new int[num_items][10];
         int[][] value_vals= new int[num_items][10];
         int temp_max_weight=10;
@@ -59,7 +18,7 @@ class Item2 implements Comparable<Item2>{
                 weight=0;
                 value=0;
                 while(d != -1 && weight<= temp_max_weight+ arr.get(d).weight){
-                    Item2 cur_item=arr.get(d);
+                    Item cur_item=arr.get(d);
                     if (temp_max_weight >= (cur_item.weight+weight)){
                         weight+=cur_item.weight;
                         value+=cur_item.value;
