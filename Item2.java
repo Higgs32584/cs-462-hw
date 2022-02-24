55a050f27ac09f6b8473703ee60e1cc9bf4028be
Higgs32584 <mjhiggins2@loyola.edu>
2022-02-24 13:09:19 -0500

    update

--- Item2.java
+++ Item2.java
@@ -52,13 +52,12 @@ class Item2 implements Comparable<Item2>{
         int weight=0;
         int value=0;
         for(int i=0;i<num_items;i++){
-            System.out.println("Item"+i+": ");
+            System.out.print("Item"+i+": ");
             for(int j=0;j<max_weight;j++){
                 d=i;
                 temp_max_weight=j;
                 weight=0;
                 value=0;
-                System.out.print("Item: "+i+"|");
                 while(d != -1 && weight<= temp_max_weight+ arr.get(d).weight){
                     Item2 cur_item=arr.get(d);
                     if (temp_max_weight >= (cur_item.weight+weight)){
@@ -68,22 +67,28 @@ class Item2 implements Comparable<Item2>{
         }
                 d--;
         }
-                if( i == 0){
-                    weight_vals[0][0]= weight;
-                    value_vals[0][0]= value;
-                    System.out.print(value+", yes");
+                if( i == 0 ){
+                    if(value == 0){
+                        System.out.print(value_vals[i][0]+", no ");
+                    }
+                    else{
+                        weight_vals[0][0]= weight;
+                        value_vals[0][0]= value;
+                        System.out.print(value+", yes ");
+                    }
         }
-                else if(value < value_vals[i-1][0]){
+                else if(value < value_vals[i-1][0] || value == 0){
                     weight_vals[i][0] = weight_vals[i-1][0];
                     value_vals[i][0] = value_vals[i-1][0];
-                    System.out.print(value_vals[i-1][0]+", no");
+                    System.out.print(value_vals[i-1][0]+", no ");
                 }
                 else{
                     weight_vals[i][0] = weight;
                     value_vals[i][0] = value;
-                    System.out.print(value+", yes");
+                    System.out.print(value+", yes ");
         }
     }
+    System.out.println("\n");
     }  
 
     }
